2d9d254d7f5eae0b866d87411a0a9f374de4fd27
enzoenrico <e.enrico2005@gmail.com>
2024-04-02 20:59:12 -0300

    add: read pdf

--- ai-bot.py
+++ ai-bot.py
@@ -3,15 +3,16 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.llms import Ollama
 
+#declare the used llm
+model = Ollama(
+    model="llama2",
+    temperature=0.9,
+)
+
 #load the doc for the model to read
 pdfLoader = PyPDFLoader("Resume.pdf")
 docs = pdfLoader.load()
 
-#declare the used llm
-model = Ollama(
-    model="mistral",
-    temperature=0.9,
-)
 
 chain = load_qa_chain(llm=model)
 query = "What is your name?"
