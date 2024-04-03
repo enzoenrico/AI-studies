0d79275858ea92360685b871047170a7f747cc77
Enzo Enrico <47016293+enzoenrico@users.noreply.github.com>
2024-04-03 22:44:40 +0000

    what

--- single.py
+++ single.py
@@ -1,54 +1,95 @@
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.llms.ollama import Ollama
+from langchain.prompts import (
+    ChatPromptTemplate,
+    FewShotChatMessagePromptTemplate,
+)
+
+import os
+
+import threading
 
 
-# load the document as before
-loader = PyPDFLoader('Resume.pdf')
-documents = loader.load()
-print("loaded document")
+def chonker():
+    documents = []
+    for file in os.listdir("docs"):
+        if file.endswith(".pdf"):
+            pdf_path = "./docs/" + file
+            loader = PyPDFLoader(pdf_path)
+            documents.extend(loader.load())
+        elif file.endswith(".docx") or file.endswith(".doc"):
+            doc_path = "./docs/" + file
+            loader = Docx2txtLoader(doc_path)
+            documents.extend(loader.load())
+        elif file.endswith(".txt"):
+            text_path = "./docs/" + file
+            loader = TextLoader(text_path)
+            documents.extend(loader.load())
 
-# we split the data into chunks of 1,000 characters, with an overlap
-# of 200 characters between the chunks, which helps to give better results
-# and contain the context of the information between chunks
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-documents = text_splitter.split_documents(documents)
-print("split document into chunks")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    chunked_documents = text_splitter.split_documents(documents)
+    print("chunked documents")
+    return chunked_documents
+
 
 # we create our vectorDB, using the OpenAIEmbeddings tranformer to create
 # embeddings from our text chunks. We set all the db information to be stored
 # inside the ./data directory, so it doesn't clutter up our source files
-vectordb = Chroma.from_documents(
-  documents,
-  embedding=OllamaEmbeddings(model='mistral', temperature=0.9),
-  persist_directory='./data'
-)
-vectordb.persist()
-print('created vectorDB')
+def createvectorDB(docs, db):
+    print(docs)
+    print("initializing vector")
+    vectordb = Chroma.from_documents(
+        docs,
+        embedding=OllamaEmbeddings(model="mistral", temperature=0.9),
+        persist_directory="./data",
+    )
+    db = vectordb
+    print("vector initialized")
+    vectordb.persist()
+    print("created vectorDB")
+    return
 
 
-qa_chain = ConversationalRetrievalChain.from_llm(
-    llm=Ollama(model='mistral', temperature=0.9),
-    retriever=vectordb.as_retriever(search_kwargs={'k': 7}),
-    return_source_documents=True
-)
-print('created QA chain')
+def createQAchain(vectordb):
+    """
+    Creates a conversational retrieval chain for question-answering.
+
+    Returns:
+        qa_chain (ConversationalRetrievalChain): The created QA chain.
+    """
+    print("creating QA chain")
+    qa_chain = ConversationalRetrievalChain.from_llm(
+        llm=Ollama(model="mistral", temperature=0.9, verbose=True),
+        retriever=vectordb.as_retriever(search_kwargs={"k": 2}),
+        return_source_documents=True,
+    )
+    print("created QA chain")
+    return qa_chain
+
 
 chat_hist = []
+db = None
+vectorThread = threading.Thread(target=createvectorDB, args=(chonker(), db))
+vectorThread.start()
 
+vectorThread.join()
+if db:
+    chain = createQAchain(db)
+else:
+    print("Error creating vectorDB")
 while True:
-    #execute queries in qa chain
-    userInp = input('[+] Write your query here: \n')
-    if userInp == 'q':
-        print('exiting...')
-        break
-    anws = qa_chain({'question': userInp, 'chat_history': chat_hist})
-    print("[Mistral]" + anws['answer'])
-    chat_hist.append((userInp, anws['answer']))
-    print('\n\n')
-# # we can now execute queries against our Q&A chain
-# result = qa_chain.invoke({'query': 'Who is the CV about?'})
-# print(result['result'])
+    if db != None:
+        userInp = input("[+] Write your query here: \n")
+        if userInp == "q":
+            print("exiting...")
+            break
+        anws = chain.invoke({"question": userInp, "chat_history": chat_hist})
+        print("[Mistral]-> " + anws["answer"])
+        chat_hist.append((userInp, anws))
+        print("\n\n")
+        print("chat history: ")
+        print(chat_hist)
